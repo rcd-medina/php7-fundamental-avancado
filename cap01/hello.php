f4f51d640d3833ac79ec881c277b83449d95c9bc
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-03-19 13:20:41 -0300

    Criado o subdiretório cap01 e adicionado o arquivo hello.php. O arquivo hello.php contém o exemplo do primeiro script com HTML e PHP

---
+++ cap01/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro script em PHP</title>
+</head>
+<body>
+
+<?php
+    //
+    // O comando echo serve para "imprimir conteúdos no cliente (navegador).
+    // Neste exemplo, será impresso no navegador a mensagem Hello world! com formatação h1 do HTML.
+    echo "<h1>Hello world!</h1>";
+?>
+</body>
+</html>
