03ddc0176469144312489a858c927d337999c059
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-03-20 13:32:46 -0300

    Finalizada a aula de funções diversas do PHP para verificação de variáveis e valores

--- cap02/func_diversas.php
+++ cap02/func_diversas.php
@@ -7,6 +7,46 @@
     <title>Funções Diversas do PHP</title>
 </head>
 <body>
-    
+
+<?php
+
+// ====================================================================================================
+// Descobrindo o tipo de dados do valor de uma variável no PHP.
+$i = 1;
+$b = is_integer($i);        // is_integer() retorna true (o valor é inteiro) ou false (não é inteiro).
+echo "b = $b <br>";
+echo "Testando inteiro: ";
+var_dump(is_integer($i));   // var_dump imprime retorno de is_integer(), porém, serve para qualquer
+                            // variável, assim pode ser utilizado para debug do código.
+
+echo ("<br>");
+$s = "Texto qualquer";
+echo "Testando strings: ";  // Assim como existe o is_integer() para testar uma variável de número
+var_dump( is_string($s) );  // inteiro, também temos o is_string() que testa variável de string.
+echo ("<br>");
+
+$f = 1.55;
+echo "Testando ponto flutuante: ";
+var_dump( is_float($f) );           // Testa ponto flutuante.
+echo "<br>";
+
+$b = true;
+echo "Testando boolean: ";
+var_dump( is_bool($b) );    // Testa o tipo boolean.
+echo "<br>";
+
+$a = [];
+echo "Testa arary: ";
+var_dump( is_array($a) );   // Testa se é um array.
+echo "<br>";
+
+echo "Testando isset(): ";
+var_dump( isset($var) );    // isset() verifica se uma variável foi criada/setada com algum valor.
+echo "<br>";
+
+echo "Testando isset(): ";
+var_dump( isset($b) );      // isset() verifica se uma variável foi criada/setada com algum valor.
+echo "<br>";
+?>
 </body>
 </html>
