1ed65b1672250fade9804aaa252d4666229eebb0
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-07 19:16:24 -0300

    Arquivo cap06/calc.php configurado para buscar os recursos css, fonts, js online.

---
+++ cap06/calc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.css" type="text/css" media="screen,projection">
+    <title>Calculadora - Materialize</title>
+</head>
+<body>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.js" type="text/javascript"></script>
+</body>
+</html>
