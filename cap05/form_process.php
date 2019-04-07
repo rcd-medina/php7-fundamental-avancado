1157c6f3cf657c409f6e8c2f8f24c2703feaef23
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-07 11:23:00 -0300

    Finalizado o formulário para envio de dados via método GET.
    Finalizado o exemplo de recebimento de dados pelo PHP via método GET.

---
+++ cap05/form_process.php
@@ -0,0 +1,8 @@
+<?php
+
+//
+// Quando o formulário HTML envia os dados via método GET, o PHP recebe esses dados na
+// variável especial $_GET.
+
+// Verifica os dados enviados no método GET do formulários.
+print_r($_GET);
