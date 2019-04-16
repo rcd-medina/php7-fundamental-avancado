72e23905daa1d04ac317bda67e021340d4a56d63
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-16 18:13:01 -0300

    Finalizada a aula Cadastro em Sessão - parte 9.

--- cap10/salvar.php
+++ cap10/salvar.php
@@ -3,7 +3,15 @@
 
 session_start();
 
-if (count($_POST)) {
+if (isset($_POST['id']) && $_POST['id'] != '') {
+    $id = $_POST['id'];
+    $_SESSION['cadastropessoal'][$id]['nome']     = $_POST['nome'];
+    $_SESSION['cadastropessoal'][$id]['idade']    = $_POST['idade'];
+    $_SESSION['cadastropessoal'][$id]['telefone'] = $_POST['telefone'];
+    $_SESSION['cadastropessoal'][$id]['endereco'] = $_POST['endereco'];
+    $_SESSION['cadastropessoal'][$id]['cidade']   = $_POST['cidade'];
+    $_SESSION['cadastropessoal'][$id]['estado']   = $_POST['estado'];
+} elseif (count($_POST)) {
     var_dump($_POST);
     $_SESSION['cadastropessoal'][] = $_POST;
 }
