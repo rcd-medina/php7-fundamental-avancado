2d54d75a961365a5b4e642ab26727369bc29df56
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-17 16:45:25 -0300

    Finalizada a aula Leitura de Arquivos - Parte 2.

---
+++ cap11/leitura-arquivo-2.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$s = "arquivo.txt";
+
+if (file_exists($s)) {
+    echo "<p>Arquivo EXISTE $s.</p>";
+    
+    $f = fopen($s, 'r');
+    if ($f) {
+        /*$conteudo = fgets($f);
+        echo "<p>Conteúdo do arquivo.</p>";
+        echo $conteudo;*/
+
+        $linha = 1;
+        while (! feof($f)) {
+            $conteudo = fgets($f);
+            echo "Linha $linha: $conteudo";
+            $linha++;
+        }
+        //fclose($f);
+    }
+} else {
+    echo "<p>Arquivo NÃO EXISTE $f.</p>";
+}
+
