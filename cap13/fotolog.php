9953665df5476e8714dff04de3faa37389e732fe
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-22 11:44:58 -0300

    Finalizada a aula Projeto 3 (Folotog) - Parte 3.

---
+++ cap13/fotolog.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/materialize_v0.100.2.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>Fotolog</title>
+</head>
+<body>
+    
+    <nav class="cyan">
+        <div class="nav-wrapper">
+            <a class="brand-logo" href="#">Logotipo</a>
+            <ul class="right hide-on-med-and-down" id="nav-mobile">
+                <li><a href="#">Link 01</a></li>
+                <li><a href="#">Link 02</a></li>
+                <li><a href="#">Link 03</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <script type ="text/javascript" src="./assets/js/jquery_v3.2.1.js"></script>
+    <script src="./assets/js/materialize.min.js"></script>
+</body>
+</html>
