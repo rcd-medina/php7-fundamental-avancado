ed28d480c6b962d985c5af586f605908dfbd67f5
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-04 17:19:01 -0300

    Finalizada a classe Produto. Versão 1.0.0 Projeto: Venda - Totalmente Orientado a Objeto.

---
+++ cap16/Produto.php
@@ -0,0 +1,64 @@
+<?php
+
+
+class Produto
+{
+    private $idProduto;
+    private $nome;
+    private $preco;
+    private $departamento;
+
+    public function __construct($idProduto, $nome, $preco, $departamento) {
+        $this->idDepartamento = $departamento;
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->departamento = $departamento;
+    }
+
+    public function setIdProduto($idProduto)
+    {
+        $this->idProduto = $idProduto;
+    }
+
+    public function getIdProduto()
+    {
+        return $this->idProduto;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setDepartamento($departamento)
+    {
+        $this->departamento = $departamento;
+    }
+
+    public function getDepartamento()
+    {
+        return $this->departamento;
+    }
+
+    public function _sleep()
+    {
+        return array_keys(get_object_vars($this));
+    }
+}
+
+
