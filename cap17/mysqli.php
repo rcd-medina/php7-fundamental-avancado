5860c883639be40286547d16128f324890067442
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-09 12:25:06 -0300

    Finalizda a exemplificação de criação de uma conexão com o banco de dados e como executar uma sentença SQL INSERT no banco de dados.

--- cap17/mysqli.php
+++ cap17/mysqli.php
@@ -28,8 +28,27 @@ $mysqli = new mysqli($host, $user, $passwd, $db);
 // ====================================================================================================
 if (! $mysqli->connect_errno) {
     echo "Conexão ativa!";
-    echo "<br><br>Valor de connect_errno: ";
+    echo "<br><br>";
+    echo "Valor de connect_errno: ";
     var_dump($mysqli->connect_errno);
+    echo "<br><br>";
+
+    // ================================================================================================
+    // Cria se uma instrução INSERT SQL a ser executada no banco de dados.
+    // ================================================================================================
+    $sql = "INSERT INTO aluno (nome, idade, cidade) VALUES ('Rodrigo', 46, 'Barueri')";
+
+    // ================================================================================================
+    // Após a criação da sentença SQL, a executamos efetivamente através do método query() do objeto
+    // $mysqli. Se tudo ocorrer bem, o método retorna TRUE, caso contrário, FALSE, por isso o teste
+    // dentro do if.
+    //
+    // Obs.: Caso sejam passadas sentenças SQL do tipo SELECT, SHOW, DESCRIBE ou EXPLAIN, o método irá
+    // retornar um objeto do tipo mysqli_result com os dados retornados pela sentença.
+    // ================================================================================================
+    if ($mysqli->query($sql)) {
+        echo "<p>Registro inserido com sucesso!</p>";
+    }
 } else {
     echo "Erro na conexão!";
     echo "<br><br>Valor de connect_errno: ";
