6e6e7057f72359143d989d2228247f287b7acdb2
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-10 17:41:50 -0300

    Finalizada a aula MySQLi - Utilizando o bind_param() na inserção.
    Código comentado.

--- cap17/mysqli5.php
+++ cap17/mysqli5.php
@@ -55,81 +55,41 @@ if (! $mysqli->connect_errno) {
     var_dump($mysqli->connect_errno);
     echo "<br><br>";
 
-    // ================================================================================================
-    // Cria se uma instrução INSERT SQL a ser executada no banco de dados.
-    // ================================================================================================
-    //
-    // $sql = "INSERT INTO aluno (nome, idade, cidade) VALUES ('Rodrigo', 46, 'Barueri')";
-    //
+    // ============================================================================================
+    // Sentença SQL para execução de um INSERT na tabela.
+    // Em seguida prepara a sentença SQL para ser executada, lembrando que todos os pontos de ?
+    // interrogação serão trocados quando o método bind_param do objeto $stmt for chamados.
+    // ============================================================================================
+    $sql = "INSERT INTO aluno (nome, idade, cidade) VALUES (?, ?, ?)";
+    $stmt = $mysqli->prepare($sql);
+    
+    // ============================================================================================
+    // Primeiro verifica se o objeto $stmt não é nullo.
+    // Em seguida utiliza se o método bind_param() para vincular os valores com o tipo de dado
+    // a ser utilizado para cada campo ? (interrogação) na sentença SQL, neste exemplo seria isso:
+    // string (s) para nome, integer (i) para idade e string (s) para cidade, logo, sis.
+    // ============================================================================================
+    if (isset($stmt)) {
+        $stmt->bind_param('sis', $nome, $idade, $cidade);
 
-    // ================================================================================================
-    // Cria se uma instrução UPDATE SQL a ser executada no banco de dados.
-    // ================================================================================================
-    //
-    // $sql = "UPDATE aluno SET idade = idade + 1 WHERE idaluno = 7";
-    //
+        // ========================================================================================
+        // Definição dos valores das variáveis.
+        // Por fim executa a sentença SQL no banco de dados com uma chamada ao método execute().
+        // ========================================================================================
+        $nome = "Fabio Machado";
+        $idade = 22;
+        $cidade = "Indaiatuba";
+        $stmt->execute();
 
-    // ================================================================================================
-    // Após a criação da sentença SQL, a executamos efetivamente através do método query() do objeto
-    // $mysqli. Se tudo ocorrer bem, o método retorna TRUE, caso contrário, FALSE, por isso o teste
-    // dentro do if.
-    //
-    // Obs.: Caso sejam passadas sentenças SQL do tipo SELECT, SHOW, DESCRIBE ou EXPLAIN, o método irá
-    // retornar um objeto do tipo mysqli_result com os dados retornados pela sentença.
-    // ================================================================================================
-    //
-    // if ($mysqli->query($sql)) {
-    //    echo "<p>Registro inserido com sucesso!</p>";
-    // }
-    //
+        $nome = "Ana Conceição";
+        $idade = 21;
+        $cidade = "Ferraz de Vasconcellos";
+        $stmt->execute();
 
-    //
-    // if ($mysqli->query($sql)) {
-    //    echo "<p>Registro alterado com sucesso!</p>";
-    // }
-    //
-
-    // ================================================================================================
-    // LEMBRANDO:
-    // Quando executamos uma sentença SQL que retorna linhas de dados (registros), o método query irá
-    // retornar um objeto do tipo mysqli_result contendo os registros.
-    // Para recuperar os registros precisamos chamar os métodos apropriados deste objeto.
-    // ================================================================================================
-    $sql = "SELECT * FROM aluno";
-    $res = $mysqli->query($sql);
-
-    if (isset($sql)) {
-        // ============================================================================================
-        // O objeto $res (mysqli_result) possui uma propriedade chamada num_rows que armazena a, em 
-        // forma de inteiro, a quantidade de resgitros recuperados.
-        // ============================================================================================
-        echo "<p>Foram encontrados " . $res->num_rows . " registros.";
-
-        // ============================================================================================
-        // Após o resultado ser armazenado na variável $res, será utilizado o método fetch_assoc() para
-        // recuperar cada registro encontrado no banco de dados.
-        //
-        // O laço while(), aqui apresentando, funcionará da seguinte maneira: O método fetch_assoc()
-        // do objeto $res (mysqli_result) retornará cada registro, em forma de um array associativo, e
-        // esse array será armazenado na variável $row (linha). Quando o método fetch_assoc() chegar
-        // ao fim, ou seja, ler todos os registros, ele retornará o valor null para a variável $row,
-        // nesse momento o laço será finalizado.
-        //
-        // Já que cada registro (linha) é retornado como um array, basta acessar o valor de cada
-        // coluna através do nome da caluna, já que o método fetch_assoc() retorna um array associativo
-        // sendo que cada chave é o nome da coluna da tabela, na mesma ordem, e o valor é o dados.
-        //
-        // Ex.: $row['idaluno'], $row['nome'], e assim por diante.
-        // ============================================================================================
-        echoTableHead();
-        while ($row = $res->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row['idaluno'] . "</td>";
-            echo "<td>" . $row['nome'] . "</td>";
-            echo "<td>" . $row['idade'] . "</td>";
-            echo "<td>" . $row['cidade'] . "</td>";
-        }
-        echoTableFoot();
+        $nome = "Fátima Bernardes";
+        $idade = 54;
+        $cidade = "São Paulo";
+        $stmt->execute();
     }
 
 } else {
