5b887722e48915069eefa900adb94942d53f4d54
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-11 15:02:11 -0300

    Finalizada a aula PDO - Selecionado registros.
    Código comentado.

--- cap18/pdo2.php
+++ cap18/pdo2.php
@@ -28,6 +28,38 @@ $dsn = "mysql:host=$host; dbname=$dbname; charset=$charset";
 try {
     $pdo = new PDO($dsn, $user, $passwd);
     echo "<p>Conectado ao banco de dados: $dbname</p>";
+    echo "<br>";
+
+    $sql = 'SELECT * FROM aluno';
+
+    // ================================================================================================
+    // No acesso a banco de dados também existe o método prepare() para preparar as sentenças SQL a
+    // serem executadas no banco de dados, assim como o mysqli, e funciona da mesma maneira.
+    // 
+    // Também poderia ser utilizado $stmt->query(), nesse caso não seria necessário utilizar os dois
+    // métodos prepare() e execute().
+    //
+    // Ex.:
+    // $stmt = $pdo->query('SELECT * FROM aluno');
+    // ================================================================================================
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    // ================================================================================================
+    // O método fetch() retorna cada registro encontrado em formato de array indexado por números e
+    // também por chaves associativas, sendo que cada chave é o nome da tabela no banco de dados na
+    // mesma ordem estrutural da tabela, logo.
+    // $row[0] e $row['idaluno'] possuem o mesmo valor, no caso, o id do aluno retornado.
+    //
+    // Quando não houveram mais registros a serem retornados, será retorno o valor null.
+    // ================================================================================================
+    while ($row = $stmt->fetch()) {
+        echo "<p>ID Aluno: {$row['idaluno']}</p>";
+        echo "<p>Nome Aluno: {$row['nome']}</p>";
+        echo "<p>Idade Aluno: {$row['idade']}</p>";
+        echo "<p>Cidade Aluno: {$row['cidade']}</p>";
+        echo "<hr>";
+    }
 } catch (PDOException $ex) {
     echo "Erro: " . $ex->getMessage();
 }
